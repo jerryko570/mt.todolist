bbaaa0da0c07a1c4321d6a55598565242c65da27
jerryko570 <jerry.narae@gmail.com>
2025-09-12 22:10:53 +0900

    BasicToDo 2번코드 작성 완료

--- src/step1/BasicTodo.jsx
+++ src/step1/BasicTodo.jsx
@@ -9,18 +9,22 @@ const BasicTodo = () => {
     { job: "산책하기", state: "미완료" },
   ]);
   // TODO: 1. 할 일 입력을 위한 state 생성
-
+  const [todo, setTodo] = useState("");
   // TODO: 2. 할 일 추가 함수 구현
-  const addTodo = () => { };
+  const addTodo = () => {
+    if (todo.trim() === "") return;
+    const 
+      setTodo([...todo]);
+  };
 
   // TODO: 3. 완료/미완료 토글 함수 구현
-  const toggleTodoState = (index) => { };
+  const toggleTodoState = (index) => {};
 
-  // TODO: 4. 할 일 삭제 함수 구현
-  const deleteTodo = (index) => { };
+  // TODO: 4. 할 일 삭제 함수 구현 (여기까지 구현하기!)
+  const deleteTodo = (index) => {};
 
   // TODO: 5. Enter 키 처리 함수 (한국어 입력 고려)
-  const handleKeyDown = (e) => { };
+  const handleKeyDown = (e) => {};
 
   return (
     <div className="todo-container">
@@ -31,9 +35,10 @@ const BasicTodo = () => {
             className="todo-input"
             type="text"
             placeholder="새로운 할 일을 입력하세요..."
+            value={todo}
             /* TODO: 7. input 값 채우기 */
             // value={} // 1번의 state 적용
-            onChange={(e) => { }} // 1번의 setState 적용
+            onChange={(e) => setTodo(e.target.value)} // 1번의 setState 적용
             onKeyDown={handleKeyDown}
           />
           <button className="add-button" onClick={addTodo}>
